2528ebf88bd5aa2afee10788029d093b415f690e
mohammedafsalk <techafs456@gmail.com>
2024-01-12 09:54:23 +0530

    client side initial setup added

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dropdown from "./components/Dropdown";
+
+export default function App() {
+  return (
+    <>
+      <div className=" flex justify-center items-center my-10 px-auto sm:px-96">
+        <Dropdown />
+      </div>
+    </>
+  );
+}
